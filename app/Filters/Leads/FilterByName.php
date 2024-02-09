6864bdfbaf8ec0969742b4b65ec73354b3f026d0
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2024-02-09 14:19:44 +0000

    (fix): filters

--- app/Filters/Leads/FilterByName.php
+++ app/Filters/Leads/FilterByName.php
@@ -2,7 +2,9 @@
 
 namespace App\Filters\Leads;
 
+use App\Imports\Leads\LeadsImport;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
 use Spatie\QueryBuilder\Filters\Filter;
 
 class FilterByName implements Filter
@@ -17,16 +19,19 @@ class FilterByName implements Filter
      */
     public function __invoke(Builder $query, $value, string $property): void
     {
-        $names = explode(' ', $value);
+        if (Str::contains($value, " ")) {
+            $name = (new LeadsImport)->split_name($value);
 
-        if (count($names) == 2) {
-            $firstPart = $names[0];
-            $lastPart = $names[1];
-
-            $query->where('first_name', 'like', '%' . $firstPart . '%')
-                ->orWhere('last_name', 'like', '%' . $lastPart . '%');
+            $query->where('first_name', 'like', '%' . $name['first_name'] . '%')
+                ->when($name['middle_name'] !== '', function ($query) use ($name) {
+                    $query->orWhere('middle_name', 'like', '%' . $name['middle_name'] . '%');
+                })
+                ->when($name['last_name'] !== '', function ($query) use ($name) {
+                    $query->orWhere('last_name', 'like', '%' . $name['last_name'] . '%');
+                });
         } else {
             $query->where('first_name', 'like', '%' . $value . '%')
+                ->orWhere('middle_name', 'like', '%' . $value . '%')
                 ->orWhere('last_name', 'like', '%' . $value . '%');
         }
     }
