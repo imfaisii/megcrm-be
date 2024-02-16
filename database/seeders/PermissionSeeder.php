fbe13cbb28b56c2bddc0924f9d9a1ea6021b188a
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2024-02-16 19:16:05 +0000

    (fix): removed extra added permissions

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -11,21 +11,6 @@ use function App\Helpers\get_permissions_by_routes;
 
 class PermissionSeeder extends Seeder
 {
-    protected $customPermissions = [
-        'leads' => [
-            [
-                'name' => 'assigned-leads',
-                'method' => 'index'
-            ]
-        ],
-        'lead-jobs' => [
-            [
-                'name' => 'assigned-jobs',
-                'method' => 'index'
-            ]
-        ],
-    ];
-
     /**
      * Run the database seeds.
      */
@@ -34,23 +19,7 @@ class PermissionSeeder extends Seeder
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
         $permissionsByRoutes = get_permissions_by_routes();
-
-        foreach ($this->customPermissions as $key => $value) {
-            if (!isset($permissionsByRoutes[$key])) {
-                $permissionsByRoutes[$key] = $value;
-            } else {
-                if (is_array($permissionsByRoutes[$key])) {
-                    if (is_array($value)) {
-                        $permissionsByRoutes[$key] = array_merge($permissionsByRoutes[$key], $value);
-                    } else {
-                        $permissionsByRoutes[$key][] = $value;
-                    }
-                } else {
-                    $permissionsByRoutes[$key] = $value;
-                }
-            }
-        }
-
+        
         foreach ($permissionsByRoutes as $module => $subModules) {
             Permission::firstOrCreate(['name' => $module, 'is_module' => true, 'guard_name' => 'sanctum']);
 
