8ae5b2c8111da71a36f05009a3e86851cc9f2073
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2024-02-17 12:06:56 +0000

    (feat): dropbox added

--- app/Actions/Users/StoreUserAction.php
+++ app/Actions/Users/StoreUserAction.php
@@ -4,31 +4,29 @@ namespace App\Actions\Users;
 
 use App\Actions\Common\AbstractCreateAction;
 use App\Events\Users\NewUserCreated;
-use App\Mail\Mails\Users\NewUserCredentialsMail;
 use App\Models\User;
 use Exception;
-use Illuminate\Support\Facades\Mail;
 
 class StoreUserAction extends AbstractCreateAction
 {
-    protected string $modelClass = User::class;
+  protected string $modelClass = User::class;
 
-    public function create(array $data): User
-    {
-        /** @var User $user */
-        $data['created_by_id'] = auth()->id();
+  public function create(array $data): User
+  {
+    /** @var User $user */
+    $data['created_by_id'] = auth()->id();
 
-        $user = parent::create($data);
+    $user = parent::create($data);
 
-        if (isset($data['roles'])) {
-            $user->syncRoles($data['roles']);
-        }
-
-        try {
-            event(new NewUserCreated(user: $user, password: $data['password']));
-        } catch (Exception $e) {
-        }
-
-        return $user;
+    if (isset($data['roles'])) {
+      $user->syncRoles($data['roles']);
     }
+
+    try {
+      event(new NewUserCreated(user: $user, password: $data['password']));
+    } catch (Exception $e) {
+    }
+
+    return $user;
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,17 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+Route::get('/dropbox', function () {
+    $redirect = "http://localhost:8000/dropbox/redirect";
+    $url = "https://www.dropbox.com/oauth2/authorize?client_id=8vda4d31bbpfvxm&response_type=code&redirect_uri=$redirect&token_access_type=offline";
+
+    return redirect($url);
+});
+
+Route::get('/dropbox/redirect', function () {
+    dd(request()->all());
+});
+
 Route::prefix('aircall')->as('aircall_')->group(function () {
     Route::post('check/webhook', AirCallWebhookController::class)->name("webhook");
 });
