9044074fb45a6645cb6c3d3d56542d3fdb61b7f2
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2024-03-07 16:56:26 +0000

    (fix): minor-update.

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -4,22 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Sms\SendSmsRequest;
 use App\Models\Lead;
-use App\Services\TwilioService;
+use App\Notifications\Sms\TwilioMessageNotification;
 
 class SmsController extends Controller
 {
     public function sendSmsToLead(SendSmsRequest $request, Lead $lead)
     {
-        $twilioService = new TwilioService(
-            app()->isLocal()
-                ? config('services.twilio.number')
-                : $lead->leadGenerator->sender_id
-        );
-        
-        $twilioService->message(
-            $lead->phone_number_formatted,
-            $request->body
-        );
+        $lead->notify(new TwilioMessageNotification($request->body));
 
         return $this->success();
     }
