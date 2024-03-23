658e08f9865d83bbf9bdb91b1d24a9f7cfb54b83
Haamzaaay <85665625+Haamzaaay@users.noreply.github.com>
2024-03-23 12:31:44 +0000

    lead actual post code fix

--- app/Observers/LeadObserver.php
+++ app/Observers/LeadObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Lead;
 
+use function App\Helpers\formatPostCodeWithSpace;
 use function App\Helpers\generateUniqueRandomString;
 
 class LeadObserver
@@ -11,5 +12,6 @@ class LeadObserver
     public function creating(Lead $lead): void
     {
         $lead->reference_number = generateUniqueRandomString();
+        $lead->actual_post_code = formatPostCodeWithSpace($lead->post_code);
     }
 }
