36e9ebb27029c224415f41f69639f92d9e4c4bca
Haamzaaay <85665625+Haamzaaay@users.noreply.github.com>
2024-04-18 10:55:04 +0100

    fix

--- app/Actions/Users/FindUserAction.php
+++ app/Actions/Users/FindUserAction.php
@@ -18,8 +18,8 @@ class FindUserAction extends AbstractFindAction
     public function findOrFail($primaryKey, array $columns = ['*']): BaseModel
     {
         $user = $this->getQuery()->findOrFail($primaryKey, $columns)->makeVisible(['air_caller_id', 'aircall_email_address']);
-
         $user->load('installationTypes');
+        $user->installationTypes = $user?->installationTypes?->pluck('id')?->all() ?? [];
         $user['documents'] = $user->getMedia(MediaCollectionEnum::DOCUMENTS);
         $user['company_documents'] = $user->getMedia(MediaCollectionEnum::COMPANY_DOCUMENTS)->toArray();
 
