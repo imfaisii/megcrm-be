514550852b4d60ec7b9b13522b9a8be8c6d03851
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2024-04-25 15:35:19 +0100

    (fix): minor updates

--- app/Actions/Leads/GetOtherSitesLinkAction.php
+++ app/Actions/Leads/GetOtherSitesLinkAction.php
@@ -56,78 +56,84 @@ class GetOtherSitesLinkAction
             $client = new Client();
             $client->setServerParameter('HTTP_USER_AGENT', fake()->userAgent());
             $crawler = $client->request('GET', "{$baseUrl}/find-a-certificate/search-by-postcode?postcode={$lead->post_code}");
-            $addressLink = $this->getAddressLink($crawler, is_array($lead['raw_api_response']['address']) ? $lead['raw_api_response']['address'][0] : $lead['raw_api_response']['address']);
+            $res = $client->getResponse();
 
-            if (!is_null($addressLink)) {
-                $crawler = $client->request('GET', "{$baseUrl}{$addressLink['link']}");
+            if ($res->getStatusCode() === 200) {
+                $addressLink = $this->getAddressLink($crawler, is_array($lead['raw_api_response']['address']) ? $lead['raw_api_response']['address'][0] : $lead['raw_api_response']['address']);
 
-                // epc energy grade
-                $epcEnergyClass = "p.epc-rating-result";
-                $epcEnergyNode = $crawler->filter("$epcEnergyClass");
+                if (!is_null($addressLink)) {
+                    $crawler = $client->request('GET', "{$baseUrl}{$addressLink['link']}");
 
-                $data['epc_energy'] = null;
-                if ($epcEnergyNode->count() > 0) {
-                    $epcEnergyData = $epcEnergyNode->text();
-                    $data['epc_energy'] = $epcEnergyData;
-                }
+                    // epc energy grade
+                    $epcEnergyClass = "p.epc-rating-result";
+                    $epcEnergyNode = $crawler->filter("$epcEnergyClass");
 
-                // find all elements with class "govuk-summary-list__row"
-                $crawler->filter('.govuk-summary-list__row')->each(function ($row) use (&$data) {
-                    $key = $row->filter('.govuk-summary-list__key')->text();
-                    $value = trim($row->filter('.govuk-summary-list__value')->text());
-
-                    // Add the key-value pair to the results array
-                    $data[Str::snake(Str::replace("’", "", $key))] = $value;
-                });
-
-                // find the table element
-                $data['features'] = [];
-                $tableNode = $crawler->filter('table.govuk-table');
-                if ($epcEnergyNode->count() > 0) {
-                    $tableNode->filter('tbody.govuk-table__body tr')->each(function ($row) use (&$data) {
-                        $data['features'][] = [
-                            'feature' => $row->filter('th.govuk-table__cell')->eq(0)->text(),
-                            'description' => $row->filter('td.govuk-table__cell')->eq(0)->text(),
-                            'rating' => $row->filter('td.govuk-table__cell')->eq(1)->text()
-                        ];
-                    });
-                }
-
-                // rating current and potential
-                $data['rating_current'] = null;
-                $data['rating_potential'] = null;
-                $ratingCurrentSelector = "svg.rating-current.rating-label";
-                $ratingPotentialSelector = "svg.rating-potential.rating-label";
-                try {
-                    $ratingCurrentNode = $crawler->filter($ratingCurrentSelector);
-                    if ($ratingCurrentNode->count() > 0) {
-                        $data['rating_current'] = $ratingCurrentNode->text();
+                    $data['epc_energy'] = null;
+                    if ($epcEnergyNode->count() > 0) {
+                        $epcEnergyData = $epcEnergyNode->text();
+                        $data['epc_energy'] = $epcEnergyData;
                     }
 
-                    $ratingPotentialNode = $crawler->filter($ratingPotentialSelector);
-                    if ($ratingPotentialNode->count() > 0) {
-                        $data['rating_potential'] = $ratingPotentialNode->text();
-                    }
-                } catch (Exception $e) {
-                    Log::error("Potential Current error at line " . $e->getLine() . ": " . $e->getMessage() . "Page title: " . $crawler->filter('title')->text());
-                }
+                    // find all elements with class "govuk-summary-list__row"
+                    $crawler->filter('.govuk-summary-list__row')->each(function ($row) use (&$data) {
+                        $key = $row->filter('.govuk-summary-list__key')->text();
+                        $value = trim($row->filter('.govuk-summary-list__value')->text());
 
-                // change can make ( improvements )
-                $data['improvements'] = [];
-                $improvements = $crawler->filter('.epb-recommended-improvements');
-                if ($improvements->count() > 0) {
-                    $improvements->filter('h3')->each(function ($h3) use (&$data) {
-                        $data['improvements'][] = $h3->text();
+                        // Add the key-value pair to the results array
+                        $data[Str::snake(Str::replace("’", "", $key))] = $value;
                     });
+
+                    // find the table element
+                    $data['features'] = [];
+                    $tableNode = $crawler->filter('table.govuk-table');
+                    if ($epcEnergyNode->count() > 0) {
+                        $tableNode->filter('tbody.govuk-table__body tr')->each(function ($row) use (&$data) {
+                            $data['features'][] = [
+                                'feature' => $row->filter('th.govuk-table__cell')->eq(0)->text(),
+                                'description' => $row->filter('td.govuk-table__cell')->eq(0)->text(),
+                                'rating' => $row->filter('td.govuk-table__cell')->eq(1)->text()
+                            ];
+                        });
+                    }
+
+                    // rating current and potential
+                    $data['rating_current'] = null;
+                    $data['rating_potential'] = null;
+                    $ratingCurrentSelector = "svg.rating-current.rating-label";
+                    $ratingPotentialSelector = "svg.rating-potential.rating-label";
+                    try {
+                        $ratingCurrentNode = $crawler->filter($ratingCurrentSelector);
+                        if ($ratingCurrentNode->count() > 0) {
+                            $data['rating_current'] = $ratingCurrentNode->text();
+                        }
+
+                        $ratingPotentialNode = $crawler->filter($ratingPotentialSelector);
+                        if ($ratingPotentialNode->count() > 0) {
+                            $data['rating_potential'] = $ratingPotentialNode->text();
+                        }
+                    } catch (Exception $e) {
+                        Log::error("Potential Current error at line " . $e->getLine() . ": " . $e->getMessage() . "Page title: " . $crawler->filter('title')->text());
+                    }
+
+                    // change can make ( improvements )
+                    $data['improvements'] = [];
+                    $improvements = $crawler->filter('.epb-recommended-improvements');
+                    if ($improvements->count() > 0) {
+                        $improvements->filter('h3')->each(function ($h3) use (&$data) {
+                            $data['improvements'][] = $h3->text();
+                        });
+                    }
+
+                    $lead->update([
+                        'epc_details' => $data
+                    ]);
+
+                    Log::info("Done {$lead->post_code} {$lead->plain_address}.");
+                } else {
+                    Log::error("Skipping {$lead->post_code} {$lead->plain_address} as no address link is found. Page title: " . $crawler->filter('title')->text());
                 }
-
-                $lead->update([
-                    'epc_details' => $data
-                ]);
-
-                Log::info("Done {$lead->post_code} {$lead->plain_address}.");
             } else {
-                Log::error("Skipping {$lead->post_code} {$lead->plain_address} as no address link is found. Page title: " . $crawler->filter('title')->text());
+                Log::error("Got a response code of: " . $res->getStatusCode() . " Page title: " . $crawler->filter('title')->text());
             }
         } catch (Exception $e) {
             Log::error("Parent EPC scrap error at line " . $e->getLine() . ": " . $e->getMessage() . "Page title: " . $crawler->filter('title')->text());
