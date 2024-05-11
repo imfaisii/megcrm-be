849a0ae2d9829defea1054416fefde1e2673e075
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2024-05-11 13:08:40 +0100

    Update Lead.php

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -141,8 +141,12 @@ class Lead extends BaseModel implements HasMedia
         $user ??= auth()->user();
         /* if he is surveyor then assigned lead gens or survey booked by him */
         if ($user->hasRole($role) && $role === RoleEnum::SURVEYOR) {
-            $query->where('created_by_id', $user->id)->orWhereHas('surveyBooking', function ($query) use ($user) {
-                $query->where('surveyor_id', $user->id);
+            $query->where(function ($query) use ($user) {
+                $query->where('created_by_id', $user->id)
+                    ->orWhere(function ($query) use ($user) {
+                        $query->join('survey_bookings', 'leads.id', '=', 'survey_bookings.lead_id')
+                            ->where('survey_bookings.surveyor_id', $user->id);
+                    });
             });
         }
 
